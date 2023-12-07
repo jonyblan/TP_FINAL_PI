1ab7e69365164bb1d35673200e998c8210aa3742
augustolanari <141185506+augustolanari@users.noreply.github.com>
2023-12-07 18:01:02 -0300

    Update salida.c

--- salida.c
+++ salida.c
@@ -1,4 +1,7 @@
-
+#include "htmlTable.h"
+#define COLQ1 4
+#define COLQ2 3
+#define COLQ1 3
 char *que1="query1.csv";
 
 FILE *pQuery1=fopen(query1, "w");
@@ -6,6 +9,7 @@ if(pQuery1==NULL){
     printf("Problema creando %s. Aborto.\n", query1);
     exit(1);
 }
+
 char * q1camp1="bikeStation";
 char * q1camp2="memberTrips";
 char * q1camp3="casualTrips";
@@ -17,23 +21,24 @@ while(hasNext1StaADT(sta)){
     fprintf(pQuery1, "%s;%d;%d;%d \n", q1->bikeStation, q1->memberTrips, q1->casualTrips, q1->totalTrips);
 }
 
-char *que2="query2.csv";
+    char *que2="query2.csv";
 
-FILE *pQuery2=fopen(query2, "w");
-if(pQuery2==NULL){
-    printf("Problema creando %s. Aborto.\n", Query2);
-    exit(1);
-}
-char * q2camp1="bikeStation";
-char * q2camp2="bikeEndStation";
-char * q2camp3="oldestDateTime";
+    FILE *pQuery2=fopen(que2, "w");
+    if(pQuery2==NULL){
+        printf("Problema creando %s. Aborto.\n", que2);
+        exit(1);
+    }
+    char * q2camp1="bikeStation";
+    char * q2camp2="bikeEndStation";
+    char * q2camp3="oldestDateTime";
+    fprintf(pQuery2, "%s;%s;%s\n",q2camp1,q2camp2,q2camp3);
 
-
-fprintf(pQuery2, "%s;%s;%s\n",q2camp1,q2camp2,q2camp3);
-while(hasNext2StaADT(sta)){
-    query2 q2=next2StaADT(sta);
-    fprintf(pQuery2, "%s;%s;%d/%d/%d %d:%d \n", q2->bikeStation, q2->bikeEndStation, q2->oldestDateTime.year,q2->oldestDateTime.mon,q2->oldestDateTime.mday,q2->oldestDateTime.hour,q2->oldestDateTime.min );
-}
+    start2StaADT(stations);
+    query2 q2;
+    while(hasNext2StaADT(stations)){
+        q2=next2StaADT(stations);
+        fprintf(pQuery2, "%s;%s;%d/%d/%d %d:%d \n", q2.bikeStation, q2.bikeEndStation, q2.oldestDateTime.tm_year,q2.oldestDateTime.tm_mon,q2.oldestDateTime.tm_mday,q2.oldestDateTime.tm_hour,q2.oldestDateTime.tm_min );
+    }
 
 
 char *que3="query3.csv";
