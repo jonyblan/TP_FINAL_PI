62a61dc7b4ef48f43de9b717990f9dad4ad4b0e4
augustolanari <141185506+augustolanari@users.noreply.github.com>
2023-12-07 17:19:03 -0300

    modifico un poco NYC

--- bikeSharingNYC.c
+++ bikeSharingNYC.c
@@ -13,32 +13,25 @@ int getLine(char ** s, FILE * file) {
 
     while ((c = fgetc(file)) != EOF && c != '\n') {
         if (w%BLOQUE == 0) {
-            s = realloc(s, (w + BLOQUE + 1));
-            if (s == NULL) {
+            (*s) = realloc((*s), (w + BLOQUE + 1));
+            if (*s == NULL) {
                 return -1;
             }
         }
-        (s)[w++] = c;
+        (*s)[w++] = c;
     }
 
     (*s)[w] = '\0';
     return 1;
 }
 
-static void printTime(struct tm t) {
-    printf("%d-%2d-%2d %2d:%2d:%2d", t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
-}
-
 void readStatFile(FILE * fileBike, stationADT station) {
 char * s = NULL;
     char * token;
-    int a = 0;
     if (getLine(&s, fileBike) == -1) {
         fprintf(stderr, "Error al leer archivo\n");
         exit(1);
     }
-    free(s);
-    s = NULL;
     while (!feof(fileBike)) {
         if ((getLine(&s, fileBike)) != -1 && (token = strtok(s, ";")) != NULL) {
             unsigned id;
@@ -47,6 +40,10 @@ char * s = NULL;
                 switch (i) {
                 case 0:
                     aux=token;
+                    break;
+                case 1:
+                    break;
+                case 2:
                     break;
                 case 3:
                     sscanf(token, "%u", &id);
@@ -59,9 +56,8 @@ char * s = NULL;
             }
             addStaADT(station, aux, id);
         }
-        free(s);
-        s = NULL;
     }
+    free(s);
 }
 
 void readBikeFile(FILE * fileBike, stationADT stations) {
@@ -71,9 +67,9 @@ void readBikeFile(FILE * fileBike, stationADT stations) {
         fprintf(stderr, "Error al leer archivo\n");
         exit(1);
     }
-    free(s);
-    s = NULL;
+    int a =0;
     while (!feof(fileBike)) {
+
         if ((getLine(&s, fileBike)) != -1 && (token = strtok(s, ";")) != NULL) {
             unsigned idStart, idEnd, isMember;
             struct tm dateStart, dateEnd;
@@ -89,7 +85,7 @@ void readBikeFile(FILE * fileBike, stationADT stations) {
                     sscanf(token, "%d-%d-%d %d:%d:%d", &(dateEnd.tm_year), &(dateEnd.tm_mon), &(dateEnd.tm_mday), &(dateEnd.tm_hour), &(dateEnd.tm_min), &(dateEnd.tm_sec));
                     break;
                 case 3:
-                    sscanf(token, "%d", &idEnd);
+                    sscanf(token, "%u", &idEnd);
                     break;
                 case 4:
                     break;
@@ -107,10 +103,10 @@ void readBikeFile(FILE * fileBike, stationADT stations) {
                 }
             }
             addTripStaADT(stations, dateStart, idStart, dateEnd, idEnd, isMember);
+            printf ("%d\n",a++);
         }
-        free(s);
-        s = NULL;
     }
+    free(s);
 }
 
 int main(int argc, char const *argv[]) {
@@ -118,7 +114,7 @@ int main(int argc, char const *argv[]) {
         fprintf(stderr, "Cantidad de parámetros erronea\n");
         exit(1);
     }
-    if (strcmp(argv[1], "bikesMON.csv") != 0 || strcmp(argv[2], "stationsMON.csv") != 0) {
+    if (strcmp(argv[1], "bikesNYC.csv") != 0 || strcmp(argv[2], "stationsNYC.csv") != 0) {
         fprintf(stderr, "Archivos pasados incorrectos\n");
         exit(1);
     }
@@ -140,14 +136,16 @@ int main(int argc, char const *argv[]) {
     char * q2camp1="bikeStation";
     char * q2camp2="bikeEndStation";
     char * q2camp3="oldestDateTime";
-
-
     fprintf(pQuery2, "%s;%s;%s\n",q2camp1,q2camp2,q2camp3);
+    start2StaADT(stations);
     while(hasNext2StaADT(stations)){
         query2 q2=next2StaADT(stations);
         fprintf(pQuery2, "%s;%s;%d/%d/%d %d:%d \n", q2.bikeStation, q2.bikeEndStation, q2.oldestDateTime.tm_year,q2.oldestDateTime.tm_mon,q2.oldestDateTime.tm_mday,q2.oldestDateTime.tm_hour,q2.oldestDateTime.tm_min );
     }
-
+    freePostReadStaADT(stations);
+    freeEndStaADT(stations);
         printf("Tiempo: %ld segundos\n", time(NULL)-t);
+
+    
     return 0;
 }
